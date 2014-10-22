0eaec860a4ef386527c3800f2d1204b2d5f85a57
Matt Keas <matthiasak@gmail.com>
2014-10-21 22:57:08 -0500

    adding Tuez, Oct 21z

--- examples/extras/setup-project.sh
+++ examples/extras/setup-project.sh
@@ -109,8 +109,8 @@ var expect = chai.expect;
 describe("Array", function(){
     describe("#indexOf()", function(){
         it("should return -1 when the value is not present", function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
+            expect([1,2,3].indexOf(5)).to.equal(-1);
+            expect([1,2,3].indexOf(0)).to.equal(-1);
         })
     })
 })
